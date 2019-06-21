02f3c69eb4417b74a6007e3b167718b63d7ef2a8
Jarius <kibeth2@outlook.com>
2019-06-20 18:27:05 -0600

    k

--- Jarvis.py
+++ Jarvis.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
+import os
 
 speech = sr.Recognizer
 
@@ -18,16 +19,18 @@ engine.setProperty("voice", voice.id)
 engine.say("Hello Sir. This is friday")
 engine.runAndWait()
 
+
 def speack_text_cmd(cmd):
     engine.say(cmd)
     engine.runAndWait()
 
+
 def read_voice_command():
     voice_text = ""
     with sr.Microphone() as source:
-       audio = speech.listen(source)
+        audio = speech.listen(source)
     try:
-       voice_text = speech.recognize_google(audio)
+        voice_text = speech.recognize_google(audio)
     except sr.UnkownValueError:
         pass
     except sr.RequestError as e:
@@ -35,20 +38,29 @@ def read_voice_command():
     return voice_text
 
 
-
 if __name__ == "__main__ ":
 
-    speak_text_cmd("Hello Small Black Star, This is Friday")
+    Speak_Text_cmd("Hello Small Black Star, This is Friday")
 
     while True:
 
         voice_note = read_voice_cmd()
 
         if "hello" in voice_note:
-            speak_text_cmd("")
+            Speak_Text_cmd("")
 
+    while False:
 
+     print("Something is not working! xD")
 
-
+   if 'hello' in voice_notes:
+        speak_text_cmd("Hello Sir, How can I help you")
+        continue
+   elif "open" in voice_note:
+       os.system('explorer C:\\{}'.format(voice_note.replace('Open , ')))
+       continue
+   elif 'bye' in voice_note:
+       speak_text_cmd("By Star, Happy to be of assistance")
+       exit()
 
 
